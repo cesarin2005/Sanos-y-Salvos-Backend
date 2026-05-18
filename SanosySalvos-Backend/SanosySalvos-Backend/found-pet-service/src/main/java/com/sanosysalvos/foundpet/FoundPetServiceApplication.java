32f7ad0007b28191d2a93eb826e8ce68504a3d11
William Castillo <wi.castillom@duocuc.cl>
2026-05-18 17:16:05 -0400

    feat(backend): codigo inicial microservicios

---
+++ SanosySalvos-Backend/SanosySalvos-Backend/found-pet-service/src/main/java/com/sanosysalvos/foundpet/FoundPetServiceApplication.java
@@ -0,0 +1,11 @@
+package com.sanosysalvos.foundpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundPetServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoundPetServiceApplication.class, args);
+    }
+}
